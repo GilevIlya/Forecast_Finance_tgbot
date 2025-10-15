f33fd9e91c5f50771a73b2747a523a3e39311259
Ilya <gilevilia2009@gmail.com>
2025-10-15 23:11:15 +0300

    sheduler changes

--- app/handlers/sheduler.py
+++ app/handlers/sheduler.py
@@ -1,7 +1,15 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from app.database import reset_weather_currency_at_midnight_db
+from app.handlers.weather import TOKEN, ADMIN_ID
+from aiogram import Bot
+
+bot = Bot(token=TOKEN)
+
+async def do_not_let_to_sleep():
+    await bot.send_message(ADMIN_ID, 'awake')
 
 async def reset_weather_currency_at_midnight():
     sheduler = AsyncIOScheduler()
+    sheduler.add_job(do_not_let_to_sleep, 'interval', minutes=1)
     sheduler.add_job(reset_weather_currency_at_midnight_db, 'cron', hour=0, minute=0)
     sheduler.start()
